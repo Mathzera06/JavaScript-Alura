833ae92e4820c0fafe18384cda2d3d255afbd155
Matheus <matheus.andrade.lopes@hotmail.com>
2023-03-30 21:58:34 -0300

    Aula do dia 30/03

---
+++ boolean.js
@@ -0,0 +1,28 @@
+// boolean true or false
+
+const usuarioLogado = true;
+const usuarioNaoLogado = false;
+
+// thuthy or falsy
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null ==> vazio ou nada
+
+let minhaLet = null
+
+let numero = 3
+let texto = 'alura'
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof minhaLet)
+
+
+
+
+// if (usuarioLogado == true) {
+//     console.log('Usuario logado com sucesso')
+// }else {
+//     console.log('Usuario não logado')
+// }
---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dados
+// booleanos
+
+//conversao implicita
+const numero = 1
+const numeroString = "1a"
+
+console.log(numero == numeroString)
+console.log(numero + numeroString)
+
+
+// conversao explicita
+console.log(numero + Number(numeroString))
+console.log(String(numero) + numeroString)
+
